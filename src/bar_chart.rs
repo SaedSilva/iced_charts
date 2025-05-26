68545d151ba3e9bff042a16fa79757196b3ca840
Saed Silva Sousa <saed.sousa@hotmail.com>
2025-05-26 17:26:42 -0300

    Enhance VerticalBarChart to display values on bars and fix bar height calculation

--- src/bar_chart.rs
+++ src/bar_chart.rs
@@ -3,12 +3,13 @@ use iced::advanced::{renderer, Widget};
 use iced::mouse::Cursor;
 use iced::widget::canvas;
 use iced::widget::canvas::{Geometry, Stroke};
-use iced::{Element, Length, Padding, Point, Rectangle, Renderer, Size, Theme};
+use iced::{Element, Length, Padding, Pixels, Point, Rectangle, Renderer, Size, Theme};
+use iced::alignment::{Horizontal, Vertical};
 
 pub struct VerticalBarChart {
     legends: Vec<String>,
     values: Vec<f32>,
-    bar_max_value: f32,
+    pub bar_max_value: f32,
     max_value: f32,
     width: Length,
     height: Length,
@@ -81,8 +82,8 @@ impl<Message> canvas::Program<Message> for VerticalBarChart {
         let bar_width = (width - self.internal_padding.horizontal() * (len_bars + 1.0)) / len_bars;
         let value_for_multiply = bar_max_height / self.max_value;
         
-        let blank_space = height - value_for_multiply * self.bar_max_value;
-        let line_space = value_for_multiply * self.bar_max_value / self.lines as f32;
+        let blank_space = height - value_for_multiply * self.max_value;
+        let line_space = value_for_multiply * self.max_value / self.lines as f32;
 
         println!("line_space: {}", line_space);
         println!("blank_space: {}", blank_space);
@@ -101,25 +102,45 @@ impl<Message> canvas::Program<Message> for VerticalBarChart {
                 }
             );
 
-            frame.stroke(&line, Stroke::default().with_color(theme.palette().text));
+            frame.stroke(&line, Stroke::default().with_color(theme.extended_palette().secondary.weak.color));
         }
 
         for (index, &value) in self.values.iter().enumerate() {
+            let top_left_x = self.internal_padding.horizontal() + (bar_width + self.internal_padding.horizontal()) * index as f32;
+            let top_left_x_mid = self.internal_padding.horizontal() + (bar_width + self.internal_padding.horizontal()) * index as f32 + bar_width / 2.0;
+            let top_left_y = bar_max_height - value * value_for_multiply + self.internal_padding.vertical();
+
+            let height = value * value_for_multiply;
+
             let bar = canvas::Path::rectangle(
                 Point {
-                    x: {
-                        let x = self.internal_padding.horizontal() + (bar_width + self.internal_padding.horizontal()) * index as f32;
-                        println!("x: {}", x);
-                        x
-                    },
-                    y: bar_max_height - value * value_for_multiply + self.internal_padding.vertical(),
+                    x: top_left_x,
+                    y: top_left_y,
                 },
                 Size {
-                    height: value * value_for_multiply,
+                    height,
                     width: bar_width,
                 },
             );
             
+            let font_size = 24.0;
+
+            let text = canvas::Text {
+                content: value.to_string(),
+                position: Point {
+                    x: top_left_x_mid,
+                    y: top_left_y - font_size
+                },
+                color: theme.palette().text,
+                size: Pixels::from(font_size),
+                line_height: Default::default(),
+                font: Default::default(),
+                horizontal_alignment: Horizontal::Center,
+                vertical_alignment: Vertical::Center,
+                shaping: Default::default(),
+            };
+
+            frame.fill_text(text);
             frame.fill(&bar, theme.palette().primary);
         }
 
